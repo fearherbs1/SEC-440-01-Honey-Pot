9da47404626f10f7adb2912dee441853ea7e268e
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2022-03-02 14:07:43 -0500

    Update graylog.bash

--- graylog.bash
+++ graylog.bash
@@ -1,20 +1,29 @@
-# Storyline: Script to install Graylog Server
+# Storyline: Script to install Graylog Server on Ubuntu Servers
 
 #!/bin/bash 
 
+# Install Prereqs
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install apt-transport-https openjdk-<version_number>-jre-headless uuid-runtime pwgen
+
+# MongoDB - set up mongodb repository
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
 sudo wget -qO- 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9DA31620334BD75D9DCB49F368818C72E52529D4' | sudo apt-key add -
 sudo systemctl daemon-reload
 sudo systemctl enable mongod.service
 sudo systemctl restart mongod.service
 sudo systemctl --type=service --state=active | grep mongod
+
+# Elasticsearch - Install elasticsearch 7 and configure yml file 
 wget -q https://artifacts.elastic.co/GPG-KEY-elasticsearch -O myKey
 sudo apt-key add myKey
 echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install elasticsearch-oss
+# set clustername and enable action.auto_create_index: false
 sudo tee -a /etc/elasticsearch/elasticsearch.yml > /dev/null <<EOT
 cluster.name: graylog
 action.auto_create_index: false
@@ -23,6 +32,8 @@ sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch.service
 sudo systemctl restart elasticsearch.service
 sudo systemctl --type=service --state=active | grep elasticsearch
+
+# Graylog - installs graylog
 sudo wget https://packages.graylog2.org/repo/packages/graylog-4.2-repository_latest.deb
 sudo dpkg -i graylog-4.2-repository_latest.deb
 sudo apt-get update && sudo apt-get install graylog-server graylog-enterprise-plugins graylog-integrations-plugins graylog-enterprise-integrations-plugins
