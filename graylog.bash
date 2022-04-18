36bb80490f530e4b96b7f806be338763226faad9
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2022-04-18 12:21:31 -0400

    Update graylog.bash

--- graylog.bash
+++ graylog.bash
@@ -3,14 +3,23 @@
 #!/bin/bash 
 
 # Install Prereqs
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install apt-transport-https openjdk-<version_number>-jre-headless uuid-runtime pwgen
+sudo yum update && sudo yum upgrade
+sudo yum install  openjdk-<version_number>-jre-headless uuid-runtime pwgen
 
 # MongoDB - set up mongodb repository
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo tee -a /etc/yum.repos.d/mongodb-org.repo > /dev/null <<EOT
+[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
+EOT
+
+sudo yum update
+sudo yum install -y mongodb-org
 
 sudo wget -qO- 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9DA31620334BD75D9DCB49F368818C72E52529D4' | sudo apt-key add -
 sudo systemctl daemon-reload
